c5a59136636bae48e12d65e1920ac0fbae17ed15
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-07-24 20:56:38 +0900

    clock

--- clock.js
+++ clock.js
@@ -1,6 +1,16 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = 
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
 
-function init () {
-
+function getTime () {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
+
+function init() {
+    getTime();
+}
+
+ init();
