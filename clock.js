dd3acd4e6f5294a223525b23576ab07fc1bd33be
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-07-25 19:28:26 +0900

    practice

--- clock.js
+++ clock.js
@@ -1,16 +1,4 @@
-const clockContainer = document.querySelector(".js-clock"),
-clockTitle = clockContainer.querySelector("h1");
-
-function getTime () {
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-}
-
-function init() {
-    getTime();
-}
-
- init();
+const title = document.getElementById("title");
+title.innerHTML = "Hi From JS";
+title.style.color = 'red';
+document.title = 'I own you now';
