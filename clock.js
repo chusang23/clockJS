5675f51ca16bca2805e399b872067b56e2764770
chusang23 <45816771+chusang23@users.noreply.github.com>
2020-07-26 19:36:06 +0900

    event handle

--- clock.js
+++ clock.js
@@ -1,4 +1,6 @@
-const title = document.getElementById("title");
-title.innerHTML = "Hi From JS";
-title.style.color = 'red';
-document.title = 'I own you now';
+const title = document.querySelector("#title");
+
+function handleClick(){
+    title.style.color = "blue";
+}
+title.addEventListener("click", handleClick);
